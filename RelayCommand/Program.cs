dac78326591451283d7b9e0e351dedaf211b5d59
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-11-09 19:31:05 +0100

    RelayCommand Implementation

--- RelayCommand/Program.cs
+++ RelayCommand/Program.cs
@@ -14,7 +14,6 @@ namespace RelayCommand
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
             Console.ReadKey();
-
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
